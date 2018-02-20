96d20ec8bb13fb1e2eb2c5614991ad32753d9d9d
hack5890 <hack5890@naver.com>
2018-02-20 10:23:45 +0900

    Test1

--- src/com/jong/s1/Main.java
+++ src/com/jong/s1/Main.java
@@ -3,9 +3,10 @@ package com.jong.s1;
 public class Main {
 
 	public static void main(String[] args) {
+
+		System.out.println("program Start");
 		SchoolController school = new SchoolController();
 		school.start();
-
 	}
 
 }
